8bc91ff6198dd7d0adbeddbaed49993a771ac687
Pawel <pawelfats@gmail.com>
2023-10-31 23:58:08 +0700

    Chek work ststem call get*

--- IPC_first_file.c
+++ IPC_first_file.c
@@ -1,8 +1,24 @@
 #include "stdio.h"
+#include <sys/types.h>
+#include <unistd.h>
+
+pid_t getpid (void);
+pid_t getppid (void);
+uid_t getuid (void);
 
 int main()
 {
-	printf("Hello world\n");
+	int pid, ppid, uid;
+	
+	pid = getpid();
+	ppid = getppid();
+	uid = getuid();
+	
+	printf("PID this programm = %d\n", pid);
+	
+	printf("PPID this programm = %d\n", ppid);
+	
+	printf("UID this programm = %d\n", uid);
     
 	return 0;
 }
